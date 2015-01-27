de962aef6aa823c288c15d4a823f762295cdda86
Sean Chen <skypemaster007@gmail.com>
2015-01-26 17:01:20 -0800

    Add handling of underscores in queries

--- indexing/query_server.py
+++ indexing/query_server.py
@@ -10,28 +10,55 @@ class QueryHandler(cmd.Cmd):
         self.do_load()
 
     def do_load(self):
+
+        # Load our `trie.pickle` file
         inputfilename = 'trie.pickle'
         with open(inputfilename, 'rb') as handle:
             self.trie = pickle.load(handle)
         return self.trie
 
     def do_query(self, query):
-        if query:
-            counter = 0
-            results = []
+
+        counter = 0
+        results = []
+
+        # Handles case where underscore is not present
+        if query and '_' not in query:
 
             # Returns an iterator over the entries in our trie whose prefixes match `query`
             iter = self.trie.iter_prefix_items(query)
             while counter < 10:
                 results.append(next(iter))
                 counter += 1
+            results = sorted(results, key=lambda t: t[1], reverse=True)
+            for i in range(10):
+                print results[i]
+
+        # Handles case where underscore is present
+        if query and '_' in query:
+            iter = self.trie.iter_prefix_items(query)
+            while counter < 10:
+                results.append(next(iter))
+                counter += 1
+            query = query.split('_')
+
+            # Query our prefix trie with the term after the underscore
+            iter = self.trie.iter_prefix_items(query[1])
+            while counter < 20:
+                results.append(next(iter))
+                counter += 1
 
             # Sort our results by value
             results = sorted(results, key=lambda t: t[1], reverse=True)
             for i in range(10):
-                print results[i]
+            print results[i]
+            return
+
         else:
             print "Please enter a valid query."
+            return
+
+
 
     def do_EOF(self, line):
         return True
